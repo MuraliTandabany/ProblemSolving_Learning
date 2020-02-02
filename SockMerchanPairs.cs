8e11ddfda74c740cb12417703d5189978fb6ae23
MuraliTandabany <40332490+MuraliTandabany@users.noreply.github.com>
2020-02-02 18:58:12 +0530

    Update SockMerchanPairs.cs

--- SockMerchanPairs.cs
+++ SockMerchanPairs.cs
@@ -1,4 +1,6 @@
-﻿using System;
+//John works at a clothing store.He has a large pile of socks that he must pair by color for sale.Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+//For example, there are  socks with colors.There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;

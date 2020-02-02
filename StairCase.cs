3dc8735483499a24f12159b0064612d13f064416
MuraliTandabany <40332490+MuraliTandabany@users.noreply.github.com>
2020-02-02 18:21:13 +0530

    Add files via upload
    Problem Solving Category

---
+++ StairCase.cs
@@ -0,0 +1,38 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace CodeTryOut
+{
+    class Staircase
+    {
+
+        // Complete the staircase function below.
+        static void staircase(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                var ash = new string('#', i);
+                Console.WriteLine(string.Format("{0}", ash.PadLeft(n)));
+            }
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            staircase(n);
+        }
+    }
+}
